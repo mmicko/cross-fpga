2198d1da962a3ee0da036af0e50eb45823d179c6
Miodrag Milanovic <mmicko@gmail.com>
2018-09-21 21:14:37 +0200

    flags for release

--- scripts/compile_icestorm.sh
+++ scripts/compile_icestorm.sh
@@ -18,18 +18,16 @@ cd $BUILD_DIR/$ICESTORM
 
 # -- Compile it
 if [ $ARCH == "darwin" ]; then
-  sed -i "s/-ggdb //;" config.mk
   sed -i "s/LDLIBS/#LDLIBS/;" iceprog/Makefile
   $CROSS make -j$J SUBDIRS="iceprog" \
             LDFLAGS="-Bstatic -pthread -L$CROSS_PREFIX/lib -Bstatic $CROSS_PREFIX/lib/libftdi1.a $CROSS_PREFIX/lib/libusb-1.0.a -Wl,-framework,IOKit -Wl,-framework,CoreFoundation -lobjc" \
-            CFLAGS="-MD -O0 -Wall -std=c99 -I$CROSS_PREFIX/include/libftdi1"
-  $CROSS make -j$J SUBDIRS="icebox icepack icemulti icepll icetime icebram"
+            CFLAGS="-MD -O2 -Wall -std=c99 -I$CROSS_PREFIX/include/libftdi1"
+  $CROSS make -j$J SUBDIRS="icebox icepack icemulti icepll icetime icebram" OPT_LEVEL=2 DBG_LEVEL=
 else
-  sed -i "s/-ggdb //;" config.mk
   $CROSS make -j$J SUBDIRS="iceprog" PKG_CONFIG_PATH=$CROSS_PREFIX/lib/pkgconfig \
             LDFLAGS="-static -pthread -L$CROSS_PREFIX/lib" \
-            CFLAGS="-MD -O0 -Wall -std=c99 -I$CROSS_PREFIX/include/libftdi1" 
-  $CROSS make -j$J SUBDIRS="icebox icepack icemulti icepll icetime icebram" STATIC=1
+            CFLAGS="-MD -O2 -Wall -std=c99 -I$CROSS_PREFIX/include/libftdi1" 
+  $CROSS make -j$J SUBDIRS="icebox icepack icemulti icepll icetime icebram" STATIC=1 OPT_LEVEL=2 DBG_LEVEL=
 fi
 
 TOOLS="icepack iceprog icemulti icepll icetime icebram"
