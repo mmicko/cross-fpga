f06d6ccfc4d8bb3cbc8a08879f52b086463f07a3
Miodrag Milanovic <mmicko@gmail.com>
2018-11-01 17:25:06 +0100

    2018.11.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE_VERSION=2018.9.0
+DATE_VERSION=2018.11.0
 
 # -- Target architectures
 ARCH=$1
@@ -8,12 +8,12 @@ TARGET_ARCHS="linux_x86_64 linux_i686 linux_armv7l linux_aarch64 windows_x86 win
 J=$(($(nproc)-1))
 
 # -- Debug flags
-BUILD_SYSTEM=1
+BUILD_SYSTEM=0
 BUILD_YOSYS=1
 BUILD_ICE40=1
 BUILD_ECP5=1
-BUILD_IVERILOG=1
-BUILD_VERILATOR=1
+BUILD_IVERILOG=0
+BUILD_VERILATOR=0
 
 # -- Store current dir
 WORK_DIR=$PWD
@@ -122,7 +122,8 @@ if [ $BUILD_SYSTEM == "1" ]; then
   print ">> Compile system"
   # -- Toolchain name
   NAME=tools-system
-  VERSION=1.2.0
+  VERSION=$DATE_VERSION
+  #VERSION=1.2.0
   # -- Create the package folders
   mkdir -p $PACKAGE_DIR/$NAME/bin
 
